3ed15ba3e63bf436e0618b455d8c9acd6432c4aa
Dev <devkatyal@protonmail.com>
2024-11-05 21:35:49 -0500

    changes to servicekey

--- config/Firebase.js
+++ config/Firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin")
-const serviceAccount = require('../serviceAccountKey.json');
+const serviceAccount = JSON.parse(Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_KEY, 'base64').toString('utf-8'));
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
